11711e9a8314b927472a806083702e5e520533ad
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-09-02 15:52:36 +0200

    feat: init the client-profile component

---
+++ angular-frontend/src/app/client-profile/client-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-profile',
+  templateUrl: './client-profile.component.html',
+  styleUrls: ['./client-profile.component.scss']
+})
+export class ClientProfileComponent {
+
+}
