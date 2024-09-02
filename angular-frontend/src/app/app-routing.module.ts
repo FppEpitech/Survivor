e9239f125145717129a4a3705a766901de84969a
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-09-02 22:02:05 +0200

    feat: handle routing to access components pages

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {  AccountManagementComponent } from './account-management/account-management.component';
+import {  ClientProfileComponent } from './client-profile/client-profile.component';
+import {  StatisticsComponent } from './statistics/statistics.component';
+
+const routes: Routes = [
+    { path: 'account-management', component: AccountManagementComponent },
+    { path: 'client-profile', component: ClientProfileComponent },
+    { path: 'statistics', component: StatisticsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {  AccountManagementComponent } from './account-management/account-management.component';
+import {  NavbarComponent } from './navbar/navbar.component';
+import {  ClientProfileComponent } from './client-profile/client-profile.component';
+import {  StatisticsComponent } from './statistics/statistics.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountManagementComponent,
+    NavbarComponent,
+    ClientProfileComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
