24b5c845f093d70c53d2060ad0e36a27b92af0fd
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-09-03 14:48:12 +0200

    feat: add charts in statistics page

--- angular-frontend/src/app/statistics-page/component/coachs/coachs.component.ts
+++ angular-frontend/src/app/statistics-page/component/coachs/coachs.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-coachs',
   templateUrl: './coachs.component.html',
-  styleUrls: ['./coachs.component.scss']
+  styleUrls: ['./coachs.component.scss'],
+  standalone: true
 })
 export class CoachsComponent {
 
--- angular-frontend/src/app/statistics-page/statistics-page.component.ts
+++ angular-frontend/src/app/statistics-page/statistics-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CoachsComponent } from './component/coachs/coachs.component';
+import { Charts } from './component/charts/charts/charts.component';
 
 @Component({
   selector: 'app-statistics-page',
   standalone: true,
-  imports: [CoachsComponent],
+  imports: [CoachsComponent, Charts],
   templateUrl: './statistics-page.component.html',
-  styleUrl: './statistics-page.component.scss'
+  styleUrls: ['./statistics-page.component.scss']
 })
 export class StatisticsPageComponent {
 
