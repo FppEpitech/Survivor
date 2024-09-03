75ea53c7389bf8afc0a867a57da3ce9ba0a0b5ca
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-03 20:43:26 +0200

    refact: remove useless /register

--- express-backend/routes/login.ts
+++ express-backend/routes/login.ts
@@ -55,24 +55,4 @@ loginRouter.post('/login', async (req: Request, res: Response) => {
     }
 });
 
-// router.post('/register', async (req, res, next) => {
-//     if (!req.body.email || !req.body.password || !req.body.name || !req.body.firstname)
-//         return res.status(400).json({ msg: "Bad parameter" });
-//         user_mail_exist(req.body.email, (exists) => {
-//         if (exists)
-//             return res.status(409).json({ msg: "Account already exists" });
-//         bcrypt.hash(req.body.password, 10, function(errhash, hash) {
-//         if (errhash)
-//             return res.status(500).json({ msg: "Internal server error" });
-//     create_user(req.body.email, hash, req.body.name, req.body.firstname,  (code, newuser) => {
-//             const expiresIn = 24 * 60 * 60;
-//             const token = jwt.sign({id: newuser.id}, process.env.SECRET, {
-//                 expiresIn: expiresIn,
-//             });
-//             return res.status(200).json({ token : token});
-//         })
-//     });  
-//   })
-// });
-
 export default loginRouter
