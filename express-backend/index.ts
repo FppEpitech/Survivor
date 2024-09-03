9ed59ba2d3068bda7b404e39ab0380b08dec91ca
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-03 19:01:46 +0200

    feat: functional login linked to legacy backend

--- express-backend/index.ts
+++ express-backend/index.ts
@@ -5,6 +5,8 @@ dotenv.config();
 import { PrismaClient } from '@prisma/client';
 import tipsRouter from './routes/tips';
 import loginRouter from './routes/login';
+import isLoggedIn from './middlewares/isLoggedIn'
+
 
 const app = express();
 const port = 3001;
@@ -12,7 +14,7 @@ const port = 3001;
 app.use(express.json());
 app.use(loginRouter)
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', isLoggedIn, (req: Request, res: Response) => {
     res.send('Hello, World!');
 });
 
