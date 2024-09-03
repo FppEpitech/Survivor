ea87cbcfc7a2d367b0e5abb527646bd939c9abd3
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-09-03 20:15:56 +0200

    refactor: delete useless tests code in AppComponent

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { TipsService } from './service/tips/tips.service';
-import { Tip } from './service/tips/tips.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -10,23 +8,4 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AppComponent {
     title = 'angular-frontend';
-
-    tips: Tip[] = [];
-    selectedTip?: Tip;
-
-    constructor(private tipsService: TipsService, private http: HttpClient) {
-        // this.http.get("/api/tips").subscribe((data) => {
-        //     console.log(data);
-        // })
-    };
-
-    ngOnInit(): void {
-        this.tipsService.getTips().subscribe(
-            (data) => {
-                this.tips = data
-                console.log(this.tips[0].id)
-            },
-            (error) => console.error('Error fetching tips', error)
-        );
-    }
 }
