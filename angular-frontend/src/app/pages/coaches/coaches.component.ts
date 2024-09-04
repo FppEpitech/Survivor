3a69daf6b9d6c7d11cbb1adc233eec04c1a9fbe5
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-09-04 21:56:51 +0200

    feat: use service EmployeesService to get employees list in coaches page

--- angular-frontend/src/app/pages/coaches/coaches.component.ts
+++ angular-frontend/src/app/pages/coaches/coaches.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee, EmployeesService } from 'src/app/service/employees/employees.service';
 
 @Component({
   selector: 'app-coaches',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./coaches.component.scss']
 })
 export class CoachesComponent {
+    employees: Employee[] = [];
 
+    constructor (private employeesService: EmployeesService) {}
+
+    ngOnInit(): void {
+        this.employeesService.getEmployees().subscribe(
+            (data) => { this.employees = data;
+                console.log(this.employees);
+            },
+            (error) => { console.error("Failed to load Employees list", error); }
+        );
+    }
 }
