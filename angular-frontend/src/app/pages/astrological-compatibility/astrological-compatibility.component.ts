f5276e4e48da0a38ce6cb3deb16333ffee46fe50
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-09-04 15:32:06 +0200

    feat: create the page astrological-compatibility

--- angular-frontend/src/app/pages/astrological-compatibility/astrological-compatibility.component.ts
+++ angular-frontend/src/app/pages/astrological-compatibility/astrological-compatibility.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-astrological-compatibility',
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./astrological-compatibility.component.scss']
 })
 export class AstrologicalCompatibilityComponent {
+    isLeftDropdownPressed = false;
+    isRightDropdownPressed = false;
 
+    // Toggles the dropdown state
+    toggleDropdown(dropdown: 'left' | 'right', event: Event) {
+      event.stopPropagation(); // Prevents the event from bubbling up the DOM tree
+      if (dropdown === 'left') {
+        this.isLeftDropdownPressed = !this.isLeftDropdownPressed;
+      } else {
+        this.isRightDropdownPressed = !this.isRightDropdownPressed;
+      }
+    }
+
+    // Closes the dropdown
+    closeDropdown(dropdown: 'left' | 'right') {
+      if (dropdown === 'left') {
+        this.isLeftDropdownPressed = false;
+      } else {
+        this.isRightDropdownPressed = false;
+      }
+    }
+
+    // Closes the dropdown if clicked outside
+    @HostListener('document:click', ['$event'])
+  clickOutside(event: MouseEvent) {
+    const dropdownLeft = document.querySelector('.dropdownLeft');
+    const dropdownRight = document.querySelector('.dropdownRight');
+
+    if (dropdownLeft && !dropdownLeft.contains(event.target as Node)) {
+      this.isLeftDropdownPressed = false;
+    }
+
+    if (dropdownRight && !dropdownRight.contains(event.target as Node)) {
+      this.isRightDropdownPressed = false;
+    }
+  }
 }
