ed4fe3f6a16210689fa18f47757692b7d5056248
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-04 23:28:31 +0200

    feat: successfully added paymenthistory router /paymenthistory

--- express-backend/index.ts
+++ express-backend/index.ts
@@ -1,9 +1,7 @@
 import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config();
-
 import {PrismaClient} from '@prisma/client';
+
 import tipsRouter from './routes/tips';
 import loginRouter from './routes/login';
 import customersRouter from './routes/customers';
@@ -11,12 +9,16 @@ import employeesRouter from './routes/employees';
 import encountersRouter from './routes/encounters';
 import eventsRouter from './routes/events';
 import clothesRouter from './routes/clothes';
+import PaymentHistoryRouter from './routes/paymentHistory'
+import authenticateToken from './middlewares/isLoggedIn';
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 app.use(loginRouter)
+
+app.use(authenticateToken); //now all routes are protected, user need to have a valid acc_token.
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/tips', tipsRouter);
@@ -25,6 +27,7 @@ app.use('/employees', employeesRouter);
 app.use('/encounters', encountersRouter);
 app.use('/events', eventsRouter);
 app.use('/clothes', clothesRouter);
+app.use('/paymenthistory', PaymentHistoryRouter);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!');
