5323900543a9f71db579d670ddbdf8c202e4aa46
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-09-04 15:28:39 +0200

    feat: link the tips with the database

--- angular-frontend/src/app/pages/tips/tips.component.ts
+++ angular-frontend/src/app/pages/tips/tips.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TipsService, Tip } from 'src/app/service/tips/tips.service';
 
 @Component({
   selector: 'app-tips',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./tips.component.scss']
 })
 export class TipsComponent {
+
+  tips: Tip[] = [];
+
+  constructor(private tipsService: TipsService) {}
+
+  ngOnInit(): void {
+    this.tipsService.getTips().subscribe(
+      (data) => {
+        console.log(data);
+        this.tips = data;
+      },
+      (error) => { console.error('Failed to load tips', error); }
+    );
+  }
 
 }
