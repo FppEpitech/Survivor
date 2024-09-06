25b2e977f9f166add737fd507b933e8b63949499
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-06 20:58:54 +0200

    feat: functional api - frontend link with docker run, handling api url

--- angular-frontend/src/environments/environment.prod.ts
+++ angular-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: ''
+  apiUrl: 'api'
 };
--- express-backend/index.ts
+++ express-backend/index.ts
@@ -15,27 +15,31 @@ require('dotenv').config();
 const app = express();
 const port = process.env.BACK_PORT || 0;
 
-app.use(express.json());
-
 app.use('/', express.static(path.join(__dirname, '../../angular-frontend/dist/angular-frontend')));
 
-app.use(loginRouter)
-app.use(require('cors')());
-app.use(require('helmet')());
+let apiRouter = express.Router();
+
+apiRouter.use(express.json());
+apiRouter.use(loginRouter)
+apiRouter.use(require('cors')());
+apiRouter.use(require('helmet')());
+
+apiRouter.use(authenticateToken); //now all routes are protected, user need to have a valid acc_token.
+
+apiRouter.use('/assets', express.static(path.join(__dirname, '../assets')));
+apiRouter.use('/tips', tipsRouter);
+apiRouter.use('/customers', customersRouter);
+apiRouter.use('/employees', employeesRouter);
+apiRouter.use('/encounters', encountersRouter);
+apiRouter.use('/events', eventsRouter);
+apiRouter.use('/clothes', clothesRouter);
+apiRouter.use('/paymenthistory', PaymentHistoryRouter);
+apiRouter.use('/compatibility', compatibilityRouter);
+
+app.use('/api', apiRouter);
 
 
 
-app.use(authenticateToken); //now all routes are protected, user need to have a valid acc_token.
-
-app.use('/assets', express.static(path.join(__dirname, '../assets')));
-app.use('/tips', tipsRouter);
-app.use('/customers', customersRouter);
-app.use('/employees', employeesRouter);
-app.use('/encounters', encountersRouter);
-app.use('/events', eventsRouter);
-app.use('/clothes', clothesRouter);
-app.use('/paymenthistory', PaymentHistoryRouter);
-app.use('/compatibility', compatibilityRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
