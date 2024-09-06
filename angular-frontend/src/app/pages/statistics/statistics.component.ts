60fbf00234f194d0ebcccbbafce0ce707101baaf
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-09-06 09:49:15 +0200

    feat: display nb of encounters per employees' customers

--- angular-frontend/src/app/pages/statistics/statistics.component.ts
+++ angular-frontend/src/app/pages/statistics/statistics.component.ts
@@ -1,3 +1,4 @@
+import { EncountersService } from './../../service/encounters/encounters.service';
 import { Component } from '@angular/core';
 import { Employee, EmployeesService } from 'src/app/service/employees/employees.service';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
@@ -14,8 +15,9 @@ export class StatisticsPageComponent {
 
     employees:Employee[] = [];
     customerResults : any = {}
+    encounterResults : any = {}
 
-    constructor(private employeesService : EmployeesService) { }
+    constructor(private employeesService : EmployeesService, private encountersService: EncountersService) { }
 
     ngOnInit(): void {
         this.employeesService.getEmployees().subscribe(
@@ -24,8 +26,18 @@ export class StatisticsPageComponent {
             for (let employee of data) {
                 this.employeesService.getCustomers(employee.id).subscribe(
                     (data) => {
-                        console.log(employee.id);
                         this.customerResults[employee.id] = data.length;
+                        for (let customer of data) {
+                            this.encountersService.getCustomerEncounters(customer.id).subscribe(
+                                (data) => {
+                                    if (this.encounterResults[employee.id])
+                                        this.encounterResults[employee.id] += data.length;
+                                    else
+                                        this.encounterResults[employee.id] = data.length;
+                                },
+                                (error) => {console.error("Failed to load encounter list", error);}
+                            )
+                        }
                     },
                     (error) => {console.error("Failed to load Employee list", error);});
             }
