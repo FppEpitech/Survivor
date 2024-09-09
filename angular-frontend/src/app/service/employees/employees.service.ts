9d7b0c31d10c099134b2bf57ed6ec74c6b8016e9
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-09-09 14:26:22 +0200

    feat: re-write service employees to handle errors

--- angular-frontend/src/app/service/employees/employees.service.ts
+++ angular-frontend/src/app/service/employees/employees.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 import { Customer } from '../customers/customers.service';
 import { environment } from 'src/environments/environment';
 
@@ -33,49 +33,84 @@ export interface EmployeeCreation {
 export class EmployeesService {
   private apiUrl = `${environment.apiUrl}/employees`;
 
+  private employees: Employee[] = [];
+  private customers: Customer[] = [];
+  private employee?: Employee;
+
   constructor(private http: HttpClient) {}
 
-  // Get all Employees
-  getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(`${this.apiUrl}/`);
-  }
+    // Get all Employees
+    async getEmployees(): Promise<Employee[]> {
+        try {
+            this.employees = await firstValueFrom(this.http.get<Employee[]>(`${this.apiUrl}/`));
+        } catch (error) {
+            console.error("Failed to load Employees list", error);
+            this.employees = [];
+        }
+        return this.employees;
+    }
 
-  // Get a single Employee by ID
-  getEmployee(id: number): Observable<Employee> {
-    return this.http.get<Employee>(`${this.apiUrl}/${id}`);
-  }
+    // Get a single Employee by ID
+    async getEmployee(id: number): Promise<Employee | undefined> {;
+        try {
+            this.employee = await firstValueFrom(this.http.get<Employee>(`${this.apiUrl}/${id}`));
+        } catch (error) {
+            console.error("Failed to load employee with id", error);
+            this.employee = undefined;
+        }
+        return this.employee;
+    }
 
-  // Get me
-  getMe(): Observable<Employee> {
-    return this.http.get<Employee>(`${this.apiUrl}/me`);
-  }
+    // Get me
+    async getMe(): Promise<Employee | undefined> {;
+        try {
+            this.employee = await firstValueFrom(this.http.get<Employee>(`${this.apiUrl}/me`));
+        } catch (error) {
+            console.error("Failed to load employee Me", error);
+            this.employee = undefined;
+        }
+        return this.employee;
+    }
 
-  // Get the customers of an Employee
-  getCustomers(id: number): Observable<Customer[]> {
-    return this.http.get<Customer[]>(`${this.apiUrl}/customers/${id}`);
-  }
+    // Get the customers of an Employee
+    async getCustomers(id: number): Promise<Customer[]> {
+        try {
+            this.customers = await firstValueFrom(this.http.get<Customer[]>(`${this.apiUrl}/customer/${id}`));
+        } catch (error) {
+            console.error("Failed to load customers list", error);
+            this.customers = [];
+        }
+        return this.customers;
+    }
 
-  // Create a new Employee
-  createEmployee(employee: EmployeeCreation): Observable<Employee> {
-    return this.http.post<Employee>(`${this.apiUrl}/`, employee, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    });
-  }
+    // Create a new Employee
+    async createEmployee(employee: EmployeeCreation): Promise<Employee | undefined> {
+        try {
+            this.employee = await firstValueFrom(this.http.post<Employee>(`${this.apiUrl}/`, employee, { headers: new HttpHeaders({'Content-Type': 'application/json'})}));
+        } catch (error) {
+            console.error("Failed to create employee", error);
+            this.employee = undefined;
+        }
+        return this.employee;
+    }
 
-  // Update an existing Employee by ID
-  updateEmployee(id: number, employee: Employee): Observable<Employee> {
-    return this.http.put<Employee>(`${this.apiUrl}/${id}`, employee, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    });
-  }
+    // Update an existing Employee by ID
+    async updateEmployee(id: number, employee: Employee): Promise<Employee | undefined> {
+        try {
+            this.employee = await firstValueFrom(this.http.put<Employee>(`${this.apiUrl}/${id}`, employee, {headers: new HttpHeaders({'Content-Type': 'application/json'})}));
+        } catch (error) {
+            console.error("Failed to create employee", error);
+            this.employee = undefined;
+        }
+        return this.employee;
+    }
 
-  // Delete a Employee by ID
-  deleteEmployee(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
-  }
-
+    // Delete a Employee by ID
+    async deleteEmployee(id: number): Promise<void> {
+        try {
+            await firstValueFrom(this.http.delete<void>(`${this.apiUrl}/${id}`));
+        } catch (error) {
+            console.error("Failed to delete employee", error);
+        }
+    }
 }
