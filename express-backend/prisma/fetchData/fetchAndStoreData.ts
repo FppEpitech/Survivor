10634006b520a88a31c6d45d8d97b7aed6515392
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-10 10:40:59 +0200

    fix: remove // useless

--- express-backend/prisma/fetchData/fetchAndStoreData.ts
+++ express-backend/prisma/fetchData/fetchAndStoreData.ts
@@ -34,8 +34,8 @@ async function get_token() {
 
 async function main() {
     let token = await get_token();
-        // await fetchTips(token);
-        // await fetchClothes(token);
+    await fetchTips(token);
+    await fetchClothes(token);
     await fetchCustomers(token);
     await fetchEmployees(token);
     await fetchEncounters(token);
