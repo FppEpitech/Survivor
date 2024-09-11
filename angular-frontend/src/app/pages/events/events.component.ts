5c3ce567b3f4059421d20f913e13dda473634a6d
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-12 01:00:54 +0200

    fix: useless tabs

--- angular-frontend/src/app/pages/events/events.component.ts
+++ angular-frontend/src/app/pages/events/events.component.ts
@@ -1,90 +1,90 @@
 import { AuthService } from 'src/app/service/auth/auth.service';
-  import { TranslocoService } from '@ngneat/transloco';
-  import { Component } from '@angular/core';
-  import * as L from 'leaflet';
-  import { Event, EventsService } from 'src/app/service/events/events.service';
-  import dayGridPlugin from '@fullcalendar/daygrid';
-  import { CalendarOptions } from '@fullcalendar/core';
-  import interactionPlugin from '@fullcalendar/interaction';
-  import en from '@fullcalendar/core/locales/en-gb';
-  import fr from '@fullcalendar/core/locales/fr';
-  import es from '@fullcalendar/core/locales/es';
-  import zh from '@fullcalendar/core/locales/zh-cn';
-  import listPlugin from '@fullcalendar/list';
-  import timeGridPlugin from '@fullcalendar/timegrid'
+import { TranslocoService } from '@ngneat/transloco';
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
+import { Event, EventsService } from 'src/app/service/events/events.service';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { CalendarOptions } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import en from '@fullcalendar/core/locales/en-gb';
+import fr from '@fullcalendar/core/locales/fr';
+import es from '@fullcalendar/core/locales/es';
+import zh from '@fullcalendar/core/locales/zh-cn';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid'
 
-  @Component({
-    selector: 'app-events',
-    templateUrl: './events.component.html',
-    styleUrls: ['./events.component.scss']
-  })
-  export class EventsComponent {
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.scss']
+})
+export class EventsComponent {
 
 
-    private map?: L.Map;
+  private map?: L.Map;
 
-    events: Event[] = [];
-    eventsToDisplay: any[] = [];
-    clang = this._tloco.getActiveLang();
-    clang_locale = this.clang == 'en' ? en : this.clang == 'fr' ? fr : this.clang == 'es' ? es : zh;
+  events: Event[] = [];
+  eventsToDisplay: any[] = [];
+  clang = this._tloco.getActiveLang();
+  clang_locale = this.clang == 'en' ? en : this.clang == 'fr' ? fr : this.clang == 'es' ? es : zh;
 
-    calendarOptions: CalendarOptions = {
-      plugins: [dayGridPlugin, interactionPlugin, listPlugin, timeGridPlugin],
-      initialView: 'dayGridMonth',
-      height: 500,
-      hiddenDays: [7],
-      locale: this.clang_locale,
-      events: this.eventsToDisplay,
-      eventClick: (data) => {
-        const description = data.event.extendedProps['description'];
-        alert(description);
-      },
-      headerToolbar: {
-        left: 'prev,next,title',
-        center: '',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-      },
-      firstDay: 0
-    };
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin, interactionPlugin, listPlugin, timeGridPlugin],
+    initialView: 'dayGridMonth',
+    height: 500,
+    hiddenDays: [7],
+    locale: this.clang_locale,
+    events: this.eventsToDisplay,
+    eventClick: (data) => {
+      const description = data.event.extendedProps['description'];
+      alert(description);
+    },
+    headerToolbar: {
+      left: 'prev,next,title',
+      center: '',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+    },
+    firstDay: 0
+  };
 
-    constructor(private eventService : EventsService, private _tloco: TranslocoService, public _auth: AuthService) {
-      this._tloco.langChanges$.subscribe(lang => {
-        this.clang = lang;
-        this.clang_locale = this.clang == 'en' ? en : this.clang == 'fr' ? fr : this.clang == 'es' ? es : zh;
-        this.calendarOptions.locale = this.clang
-        this.calendarOptions.buttonText = {
-          month: this._tloco.translate('monthTxtEv') == 'monthTxtEv' ? 'Month' : this._tloco.translate('monthTxtEv'),
-          week: this._tloco.translate('weekTxtEv') == 'weekTxtEv' ? 'Week' : this._tloco.translate('weekTxtEv'),
-          day: this._tloco.translate('dayTxtEv') == 'dayTxtEv' ? 'Day' : this._tloco.translate('dayTxtEv'),
-          list: this._tloco.translate('listTxtEv') == 'listTxtEv' ? 'List' : this._tloco.translate('listTxtEv')
-        }
-      });
-    }
-
-      ngOnInit(): void {
-          this.initMap();
-
-          this.eventService.getEvents().subscribe(events => {
-          this.events = events;
-          for (const event of events) {
-            this.addEvent(event.name, event.date, `${this._tloco.translate('maxParticipantsTxt')}: ${event.max_participants}, ${this._tloco.translate('dateTxt')}: ${event.date} --> : ${event.location_name}`);
-              L.marker([parseFloat(event.location_x), parseFloat(event.location_y)]).addTo(this.map!)
-              .bindPopup(`${this._tloco.translate('maxParticipantsTxt')}: ${event.max_participants}, ${this._tloco.translate('dateTxt')}: ${event.date} --> : ${event.location_name}`)
-              .openPopup();
-            }
-          });
+  constructor(private eventService: EventsService, private _tloco: TranslocoService, public _auth: AuthService) {
+    this._tloco.langChanges$.subscribe(lang => {
+      this.clang = lang;
+      this.clang_locale = this.clang == 'en' ? en : this.clang == 'fr' ? fr : this.clang == 'es' ? es : zh;
+      this.calendarOptions.locale = this.clang
+      this.calendarOptions.buttonText = {
+        month: this._tloco.translate('monthTxtEv') == 'monthTxtEv' ? 'Month' : this._tloco.translate('monthTxtEv'),
+        week: this._tloco.translate('weekTxtEv') == 'weekTxtEv' ? 'Week' : this._tloco.translate('weekTxtEv'),
+        day: this._tloco.translate('dayTxtEv') == 'dayTxtEv' ? 'Day' : this._tloco.translate('dayTxtEv'),
+        list: this._tloco.translate('listTxtEv') == 'listTxtEv' ? 'List' : this._tloco.translate('listTxtEv')
       }
-
-    private initMap(): void {
-      this.map = L.map('map').setView([48.86, 2.33], 13);
-
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(this.map);
-    }
-
-    addEvent(title :  string, date : string, desc : string) {
-      console.log(date);
-      this.eventsToDisplay.push({title: title, date: new Date(date), description: desc, backgroundColor:"green", color: "green"});
-    }
+    });
   }
+
+  ngOnInit(): void {
+    this.initMap();
+
+    this.eventService.getEvents().subscribe(events => {
+      this.events = events;
+      for (const event of events) {
+        this.addEvent(event.name, event.date, `${this._tloco.translate('maxParticipantsTxt')}: ${event.max_participants}, ${this._tloco.translate('dateTxt')}: ${event.date} --> : ${event.location_name}`);
+        L.marker([parseFloat(event.location_x), parseFloat(event.location_y)]).addTo(this.map!)
+          .bindPopup(`${this._tloco.translate('maxParticipantsTxt')}: ${event.max_participants}, ${this._tloco.translate('dateTxt')}: ${event.date} --> : ${event.location_name}`)
+          .openPopup();
+      }
+    });
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([48.86, 2.33], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
+
+  addEvent(title: string, date: string, desc: string) {
+    console.log(date);
+    this.eventsToDisplay.push({ title: title, date: new Date(date), description: desc, backgroundColor: "green", color: "green" });
+  }
+}
