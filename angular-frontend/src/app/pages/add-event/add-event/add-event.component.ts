93ac95ee19fdfc9a0b952c0ea3eb6c80eef9c1c8
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-11 18:43:17 +0200

    feat: clean & very similar page events/ to mockup

---
+++ angular-frontend/src/app/pages/add-event/add-event/add-event.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-event',
+  templateUrl: './add-event.component.html',
+  styleUrls: ['./add-event.component.scss']
+})
+export class AddEventComponent {
+
+}
