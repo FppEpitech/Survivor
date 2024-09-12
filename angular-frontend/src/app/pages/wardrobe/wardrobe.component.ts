bb87f56de5bfaf83a9fee88936cdd5027efdc6e0
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-09-12 10:37:07 +0200

    fix (wardrobe page): clothes images adress

--- angular-frontend/src/app/pages/wardrobe/wardrobe.component.ts
+++ angular-frontend/src/app/pages/wardrobe/wardrobe.component.ts
@@ -23,15 +23,10 @@ export class WardrobeComponent {
   clothes: Clothes[] = [];
   clothe?: Clothe;
 
-  hats_caps: Clothe[] = [];
-  tops: Clothe[] = [];
-  bottoms: Clothe[] = [];
-  shoes_list: Clothe[] = [];
-
-  hat_cap?: Clothe;
-  top?: Clothe;
-  bottom?: Clothe;
-  shoes?: Clothe;
+  hats_caps_urls: string[] = [];
+  tops_urls: string[] = [];
+  bottoms_urls: string[] = [];
+  shoes_urls: string[] = [];
 
   hat_cap_url: { url?: string } = {};
   top_url: { url?: string } = {};
@@ -44,11 +39,6 @@ export class WardrobeComponent {
   index_tops = { index: 0 };
   index_bottoms = { index: 0 };
   index_shoes = { index: 0 };
-
-  hat_cap_id: number[] = [];
-  top_id: number[] = [];
-  bottom_id: number[] = [];
-  shoes_id: number[] = [];
 
   customerSelected = false;
 
@@ -78,86 +68,40 @@ export class WardrobeComponent {
     async getClothes() {
         this.clothes = await this.customerService.getCustomerClothes(this.customer!.id);
         if (this.clothes != undefined) {
-            this.hat_cap_id = this.clothes.filter((clothe) => clothe.type == 'hat/cap').map((clothe) => clothe.id);
-            this.top_id = this.clothes.filter((clothe) => clothe.type == 'top').map((clothe) => clothe.id);
-            this.bottom_id = this.clothes.filter((clothe) => clothe.type == 'bottom').map((clothe) => clothe.id);
-            this.shoes_id = this.clothes.filter((clothe) => clothe.type == 'shoes').map((clothe) => clothe.id);
+            console.log(this.clothes);
+            this.hats_caps_urls = this.clothes.filter((clothe) => clothe.type == 'hat/cap').map((clothe) => clothe.url);
+            this.tops_urls = this.clothes.filter((clothe) => clothe.type == 'top').map((clothe) => clothe.url);
+            this.bottoms_urls = this.clothes.filter((clothe) => clothe.type == 'bottom').map((clothe) => clothe.url);
+            this.shoes_urls = this.clothes.filter((clothe) => clothe.type == 'shoes').map((clothe) => clothe.url);
 
-            this.getAllClothes().then(() => {
-                this.hat_cap = this.hats_caps.at(this.index_hats_caps.index);
-                this.top = this.tops.at(this.index_tops.index);
-                this.bottom = this.bottoms.at(this.index_bottoms.index);
-                this.shoes = this.shoes_list.at(this.index_shoes.index);
-
-                this.hat_cap_url.url = this.hat_cap?.type ? this.apiUrl + this.hat_cap.type : '';
-                this.top_url.url = this.top?.type ? this.apiUrl + this.top.type : '';
-                this.bottom_url.url = this.bottom?.type ? this.apiUrl + this.bottom.type : '';
-                this.shoes_url.url = this.shoes?.type ? this.apiUrl + this.shoes.type : '';
-            });
+            this.hat_cap_url.url = this.apiUrl + this.hats_caps_urls.at(this.index_hats_caps.index);
+            this.top_url.url = this.apiUrl + this.tops_urls.at(this.index_tops.index);
+            this.bottom_url.url = this.apiUrl + this.bottoms_urls.at(this.index_bottoms.index);
+            this.shoes_url.url = this.apiUrl + this.shoes_urls.at(this.index_shoes.index);
         }
     }
 
-  async getAllClothes(): Promise<void> {
-    try {
-      const hatCapPromises = this.hat_cap_id.map(id => this.getOneClothe(id));
-      const topPromises = this.top_id.map(id => this.getOneClothe(id));
-      const bottomPromises = this.bottom_id.map(id => this.getOneClothe(id));
-      const shoesPromises = this.shoes_id.map(id => this.getOneClothe(id));
-
-      const [hatsCaps, tops, bottoms, shoesList] = await Promise.all([
-        Promise.all(hatCapPromises),
-        Promise.all(topPromises),
-        Promise.all(bottomPromises),
-        Promise.all(shoesPromises)
-      ]);
-
-      this.hats_caps = hatsCaps;
-      this.tops = tops;
-      this.bottoms = bottoms;
-      this.shoes_list = shoesList;
-
-    } catch (error) {
-      console.error('Erreur lors de la récupération des vêtements :', error);
-    }
-  }
-
-  async getOneClothe(id: number): Promise<Clothe> {
-    try {
-      const clothe = await firstValueFrom(this.clotheService.getClothe(id));
-      return clothe;
-    } catch (error) {
-      console.error('Erreur lors de la récupération du vêtement', error);
-      throw error;
-    }
-  }
-
-  swipeItem(nextElement: boolean, itemList: Clothe[], index: {index: number}, url: { url?: string }): void {
+  swipeItem(nextElement: boolean, itemListUrls: string[], index: {index: number}, url: { url?: string }): void {
     if (nextElement) {
-      index.index = (index.index == itemList.length - 1) ? 0 : index.index + 1;
+      index.index = (index.index == itemListUrls.length - 1) ? 0 : index.index + 1;
     } else {
-      index.index = (index.index == 0) ? itemList.length - 1 : index.index - 1;
+      index.index = (index.index == 0) ? itemListUrls.length - 1 : index.index - 1;
     }
-    const item = itemList.at(index.index);
-    url.url = item?.type ? this.apiUrl + item.type! : '';
+    url.url = this.apiUrl + itemListUrls.at(index.index);
   }
 
   randomOutfit(): void {
     if (this.customerSelected === false)
       return;
-    this.index_hats_caps.index = Math.floor(Math.random() * this.hats_caps.length);
-    this.index_tops.index = Math.floor(Math.random() * this.tops.length);
-    this.index_bottoms.index = Math.floor(Math.random() * this.bottoms.length);
-    this.index_shoes.index = Math.floor(Math.random() * this.shoes_list.length);
+    this.index_hats_caps.index = Math.floor(Math.random() * this.hats_caps_urls.length);
+    this.index_tops.index = Math.floor(Math.random() * this.tops_urls.length);
+    this.index_bottoms.index = Math.floor(Math.random() * this.bottoms_urls.length);
+    this.index_shoes.index = Math.floor(Math.random() * this.shoes_urls.length);
 
-    this.hat_cap = this.hats_caps.at(this.index_hats_caps.index);
-    this.top = this.tops.at(this.index_tops.index);
-    this.bottom = this.bottoms.at(this.index_bottoms.index);
-    this.shoes = this.shoes_list.at(this.index_shoes.index);
-
-    this.hat_cap_url.url = this.hat_cap?.type ? this.apiUrl + this.hat_cap.type! : '';
-    this.top_url.url = this.top?.type ? this.apiUrl + this.top.type! : '';
-    this.bottom_url.url = this.bottom?.type ? this.apiUrl + this.bottom.type! : '';
-    this.shoes_url.url = this.shoes?.type ? this.apiUrl + this.shoes.type! : '';
+    this.hat_cap_url.url = this.apiUrl + this.hats_caps_urls.at(this.index_hats_caps.index);
+    this.top_url.url = this.apiUrl + this.tops_urls.at(this.index_tops.index);
+    this.bottom_url.url = this.apiUrl + this.bottoms_urls.at(this.index_bottoms.index);
+    this.shoes_url.url = this.apiUrl + this.shoes_urls.at(this.index_shoes.index);
   }
 
   onImageError(event: any) {
