86b2c7c046ac378b78eb2c94f29c0c90fa8849da
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-09-12 10:46:50 +0200

    feat: card for dashboard

--- angular-frontend/src/app/pages/home/home.component.ts
+++ angular-frontend/src/app/pages/home/home.component.ts
@@ -17,16 +17,13 @@ import { Employee, EmployeesService } from 'src/app/service/employees/employees.
 
 export class HomeComponent {
 
-    payments: PaymentHistory[] = [];
     customers: Customer[] = [];
-    employees: Employee[] = [];
-    coach?: Employee;
+    doingMeetings = 0;
+    customersPerCoach = 0;
 
-    paymentAccount: any = {};
-    nbCustomers: any = {};
-    nbEmployees: any = {};
-    nbEncounters: any = {};
-    genders = [0,0,0];
+    eventMonthly = 0;
+    eventWeekly = 0;
+    eventDaily = 0;
 
     constructor(
         public _auth: AuthService,
@@ -38,56 +35,6 @@ export class HomeComponent {
     ) {}
 
     async ngOnInit() {
-        if (this._auth.isManager()) {
-            await this.initPayments();
-            await this.initCustomers();
-            this.initGender(this.customers);
-            this.initEmployees();
-            this.initEncounters();
-        } else {
-            this.coach = await this.employeesService.getMe();
-            if (this.coach) {
-                this.customers = await this.employeesService.getCustomers(this.coach.id);
-                this.nbCustomers[0] = this.customers;
-                this.nbEncounters[0] = 0;
-                this.initGender(this.customers);
-                for (let customer of this.customers) {
-                    this.nbEncounters[0] += (await this.encountersService.getCustomerEncounters(customer.id)).length;
-                }
-            }
-        }
-    }
-
-    async initPayments() {
-        this.payments = await this.paymentHistoryService.getPayments();
-        this.paymentAccount[0] = 0;
-        for (let payment of this.payments) {
-            this.paymentAccount[0] += payment.amount;
-        }
-    }
-
-    async initCustomers() {
         this.customers = await this.customersService.getCustomers();
-        this.nbCustomers[0] = this.customers || 0;
-    }
-
-    initGender(customers:Customer[]) {
-        for (let customer of customers) {
-            if (customer.gender === 'Male')
-                this.genders[0] += 1;
-            else if (customer.gender === 'Female')
-                this.genders[1] += 1;
-            else
-                this.genders[2] += 1;
-        }
-    }
-
-    async initEmployees () {
-        this.employees = (await this.employeesService.getEmployees()).filter(employee => employee.work === 'Coach');
-        this.nbEmployees[0] = this.employees;
-    }
-
-    async initEncounters() {
-        this.nbEncounters[0] = await this.encountersService.getEncounters();
     }
 }
